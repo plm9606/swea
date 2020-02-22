0850740b6fcf119d0d1d644c89cf7593d27b8f1e
aram <aram.adam@navercorp.com>
2020-02-22 22:12:51 +0900

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+class Main
+{
+
+    public static void main(String[] args) {
+        Solution_1215 solution_1215 = new Solution_1215();
+        try {
+            solution_1215.solution();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }}

33b69f73c5dcd88380e509a9c943aad006e282a0
aram <aram.adam@navercorp.com>
2020-02-26 01:17:55 +0900

    1210_ver1
    - 위에서 아래로 탐색한다
    - while문을 사용해서 좌표를 탐색하고 재귀는 사용하지 않는다.
      - if, else if, else 구문을 보년 차례대로 왼쪽에 길이 있을때, 오른쪽에
    길이 있을때, 아래로 가는 길이 있을때를 검사하는데 양쪽모두에 길이
    뚫려있을 경우에는...?
    
    - 문제를 잘 읽어야 한다. 문제에 아래로가다 좌우로 뚫린 길을 만나면
    좌우로, 방향 전환 이후엔 무조건 아래로만 가야한다고 적혀있다

--- src/Solution_1210.java
+++ src/Solution_1210.java
@@ -6,48 +6,38 @@ import java.util.Scanner;
 public class Solution_1210 {
     int[] dy = {0,0,1};
     int[] dx = {-1,1,0};
-    int answer = 0;
+    int[][] arr = new int[100][100];
+    boolean FIND = false;
 
     public void solution() throws Exception
     {
-		/*
-		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
-		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
-		 */
         System.setIn(new FileInputStream("res/input1210.txt"));
 
-		/*
-		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
-		 */
         Scanner sc;
         sc = new Scanner(System.in);
 
-		/*
-		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-		*/
-
         for(int test_case = 1; test_case <= 10; test_case++)
         {
+            int answer = 0;
+            FIND = false;
+
             sc.nextInt();
-            int[][] arr = new int[100][100];
-//            int[] target = new int[2];
             for(int i=0; i<100; i++){
                 for(int j=0; j<100; j++){
                     int temp = sc.nextInt();
                     arr[i][j] = temp;
-//                    if(temp==2){
-//                        target[0]=i;
-//                        target[1]=j;
-//                    }
                 }
 
             }
 
             for(int i=0; i<100; i++){
-                if(arr[0][i]==1)find( deepCopy(arr, 100), 0, i, i);
+                if(arr[0][i]==1){
+                    find( 0, i, i);
+                    if(FIND) {
+                        answer = i;
+                        break;
+                    }
+                }
             }
 
 
@@ -58,30 +48,22 @@ public class Solution_1210 {
 
 
     }
-    public static int[][] deepCopy(int[][] original, int n) {
-        if (original == null) {
-            return null;
-        }
+    public void find(int y, int x, int start){
+        // 방문을 체크하기 위해 자료구조를 만든다
+        boolean[][] visited = new boolean[100][100];
 
-        int[][] result = new int[n][n];
-        for (int i = 0; i < original.length; i++) {
-            System.arraycopy(original[i], 0, result[i], 0, original[i].length);
-        }
-        return result;
-    }
-    public void find(int[][] arr, int y, int x, int start){
-        if(arr[y][x] == 2){
-            System.out.println("find");
-            return;
-        }
+        while (y<100){
+            visited[y][x] = true;
 
-        for(int i=0; i<3; i++){
-            if(y+dy[i] >= 0 && y+dy[i]<100 && x+dx[i] >=0 && x+dx[i]<100){
-                if(arr[y+dy[i]][x+dx[i]]==1){
-                    arr[y][x]=0;
-                    find(deepCopy(arr, 100), y+dy[i], x+dx[i], start);
-                }
+            if(arr[y][x] == 2){
+                FIND = true;
+                break;
             }
+            if(x-1 >=0 && x-1<100 && visited[y][x-1] == false && arr[y][x-1] ==1){
+                x--;
+            }else if(x+1 >=0 && x+1 < 100 && visited[y][x+1]== false && arr[y][x+1] == 1){
+                x++;
+            }else y++;
         }
     }
 }

12d2293af42e6cfecf3d342c108da39b001ad0ea
aram <plm9606@naver.com>
2020-03-01 00:58:22 +0900

    백준 7575
    **접근**
    - 첫번째 프로그램에서 만들 수 있는 모든 패턴을 만든다
    - 그 다음 프로그램에서부터는 만들어진 패턴과 매칭을 시도하고 매칭실패한 패턴은 버린다
    
    **특징**
    - kmp알고리즘을 사용해 program과 pattern의 매칭시간을 선형시간으로 단축한다
    - program을 앞에서 한번 뒤에서 한번 패턴과 매칭을 시켜보아서 거꾸로 했을때 패턴과 매칭이 되는지를 검사한다
    - 패턴을 map에 저장한 이유는 패턴을 만들때 똑같은 패턴이 만들어질 수도 있기 때문에 중복 방지를 위해 map으로 만들었다.

--- src/baekjoon/Boj_7575.java
+++ src/baekjoon/Boj_7575.java
@@ -11,7 +11,8 @@ public class Boj_7575 {
 
         int T = sc.nextInt();
         int len = sc.nextInt();
-        ArrayList<int[]> patterns = new ArrayList<>();
+        HashMap<String,int[]> patternMap = new HashMap<>();
+        HashMap<String, int[]> nextMap = new HashMap<>();
 
         for(int test =0; test<T; test++){
             int programLen = sc.nextInt();
@@ -27,15 +28,78 @@ public class Boj_7575 {
                     for(int j=0; j<len; j++){
                         pattern[j] = program[i+j];
                     }
-                    patterns.add(pattern);
+                    patternMap.put(Arrays.toString(pattern), pattern);
+                    nextMap.put(Arrays.toString(pattern), makeNextTable(pattern));
                 }
+
                 continue;
             }
 
-            for(int i=len;i<programLen; i++){
+            List<String> removeList = new ArrayList<>();
+            for(String key:patternMap.keySet()){
+                int[] pattern = patternMap.get(key);
+                int[] next = nextMap.get(key);
+                int j=0;
+                boolean MATCH = false;
+                for(int i=0; i<programLen; i++){
+                    while (j>0 && program[i] != pattern[j]){
+                        j = next[j-1];
+                    }
+                    if(program[i]==pattern[j]){
+                        if(j == pattern.length-1){
+                            System.out.println("#"+test +" find "+ (i-pattern.length+2));
+                            MATCH = true;
+                            break;
+                        }
+                        j++;
+                    }
+                }
+                j=0;
+                for(int i=programLen-1; i>=0; i--){
+                    while (j>0 && program[i] != pattern[j])
+                        j = next[j-1];
+                    if(program[i] == pattern[j]){
+                        if(j== pattern.length-1){
+                            System.out.println("#"+test +" find2 at "+i);
+                            MATCH = true;
+                            break;
+                        }
+                        j++;
+                    }
+                }
 
+                if(!MATCH) removeList.add(key);
+            }
+
+            for(String key : removeList){
+                patternMap.remove(key);
             }
 
         }
+        if(patternMap.size()>0){
+            for(String key:patternMap.keySet())
+                System.out.println(key);
+
+        }else System.out.println("NO");
     }
+
+    public static int[] makeNextTable (int[] pattern){
+        int[] next = new int[pattern.length];
+        int j=0;
+
+        for(int i=1; i<pattern.length; i++){
+            while (j>0 && pattern[i]!=pattern[j]){
+                j = next[j-1];
+
+            }
+            if(pattern[i]==pattern[j]){
+                next[i] = j+1;
+                j++;
+            }
+        }
+        return  next;
+    }
+
+
+
 }

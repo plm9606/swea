27e1407b9441b13532866fefebf03be34e530be3
aram <plm9606@naver.com>
2020-03-08 01:20:30 +0900

    Boj: 9251
    LCS(Longest Common Subsequence)는 다이나믹 프로그래밍을 이용한 알고리즘 문제 풀이에 자주 이용된다.
    이는 문자열 두개를 비교하여 순서대로 있는 가장 긴 문자의 수를 세는 것이다.
    
    배열을 만들어서 문자가 일치할 때는 좌측 상단의 값에 +1을 하여 주고,
    일치 하지 않으면 좌측 또는 상단의 값 중 더 큰값을 선택하여 준다.
    이를 끝까지 반복하여 나오는 가장 큰 수가 LCS 값이 된다.
    
    🅿️⬜️
    ⬜️[p+1]   => 문자가 일치할때
    
    ⬜️🅰️
    🅱️ [max(a,b)]. => 문자가 일치하지 않을 때
    
    https://118k.tistory.com/483

---
+++ src/baekjoon/Boj_9251.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Boj_9251 {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("res/boj9251.txt"));
+
+        Scanner sc = new Scanner(System.in);
+        String first = sc.next();
+        String second = sc.next();
+        int len = first.length();
+        int secondLen = second.length();
+
+        int[][]lcs = new int[len+1][secondLen+1];
+
+        for(int y=1;y<=len; y++){
+            for(int x=1; x<=secondLen; x++){
+                char cy = first.charAt(y-1);
+                char cx = second.charAt(x-1);
+                if(cy == cx){
+                    lcs[y][x] = lcs[y-1][x-1]+1;
+                }else {
+                   lcs[y][x] = Math.max(lcs[y-1][x], lcs[y][x-1]);
+                }
+            }
+        }
+
+        System.out.println(lcs[len][secondLen]);
+
+    }
+}

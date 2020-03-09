6676b895de463c9ce0e8d5b06a1868d6730dfe95
aram <plm9606@naver.com>
2020-03-10 02:54:19 +0900

    swea: 1245
    - 이분 탐색을 이용해 문제를 풀어야 한다.
    - 1,2,3 세개의 x좌표에 자성체가 있다면 균형점은 2개가 존재한다. (1,2)사이, (2,3)사이
    1,2 사이의 균형점을 찾아본다면
    우선 1과 2 사이의 값부터 시작해 균형점인지를 체크한다.
    임의로 균형점 왼쪽의 자성체들의 자력은 더해주고, 오른쪽의 자력은 빼준다.
    해당 mid값에서 균형을 이루면(합이 0) 해당 값이 균형점이다.
    자력의 합이 0보다 크다면 왼쪽의 자력이 더 큰 것이므로 균형점은 오른쪽으로 가야 하기 때문에 (mid+right)/2한 값을 새롭게 mid값으로 두고 계산을 한다.
    자력의 합이 음수이면 (mid+left)/2를 새로운 mid로 두고 계산한다.
    
    여기서 좌표값의 오차가 1e-12보다 작아야한다는 제약조건이 있다.
    이럴땐 여러번 돌려서 오차를 줄여나가야 한다.
    .
    int가 대충 1e9정도이므로 이것을 토대로 계산했을 때 오차가 1e-9 미만이어야 한다는 것은 계산 범위가 1e18과 같다는 것이므로 루프를 최소60번은 돌아야 한다.
    .
    라고 한다. 그래서 100번 돌림
    
    https://jaejin89.tistory.com/94

---
+++ src/Solution_1245.java
@@ -0,0 +1,58 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Solution_1245 {
+    static int[] M;
+    static int[] D;
+    static int n;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("res/input1245.txt"));
+
+
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt();
+
+        for (int test_case = 1; test_case <= 10; test_case++) {
+            int answer = 0;
+            n = sc.nextInt();
+            M = new int[n];
+            D = new int[n];
+            for (int i = 0; i < n; i++) {
+                D[i] = sc.nextInt();
+            }
+            for (int i = 0; i < n; i++) {
+                M[i] = sc.nextInt();
+            }
+
+            System.out.printf("#%d ", test_case);
+            for (int i = 0; i < n - 1; i++) {
+                System.out.printf("%.10f", solve(i, 0, (D[i] + D[i + 1]) / 2.0, D[i], D[i + 1]));
+            }
+            System.out.printf("\n");
+        }
+    }
+
+    public static double solve(int idx, int depth, double cur, double left, double right) {
+        if (depth == 100) return cur;
+        double f = 0.0;
+        double result = 0.0;
+
+        for (int i = 0; i <= idx; i++) {
+            f += (double) M[i] / Math.pow(cur - D[i], 2.0);
+        }
+        for (int i = idx + 1; i < n; i++) {
+            f -= (double) M[i] / Math.pow(D[i] - cur, 2.0);
+        }
+
+        if (f < 0) {
+            result = solve(idx, 1 + depth, (left + cur) / 2.0, left, cur);
+        } else if (f > 0) {
+            result = solve(idx, 1 + depth, (cur + right) / 2.0, cur, right);
+        } else {
+            result = cur;
+        }
+        return result;
+    }
+}

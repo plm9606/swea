c9e2b8c7626d93b4828259c40f246397a7d5e76e
aram <plm9606@naver.com>
2020-03-11 02:18:48 +0900

    swea: 9317

---
+++ src/Solution_9317.java
@@ -0,0 +1,26 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Solution_9317 {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("res/input9317.txt"));
+
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int answer = 0;
+            sc.nextInt();
+            char[] origin = sc.next().toCharArray();
+            char[] dictation = sc.next().toCharArray();
+
+            for (int i = 0; i < origin.length; i++) {
+                if (origin[i] == dictation[i]) answer++;
+            }
+            System.out.printf("#%d %d\n", test_case, answer);
+
+        }
+    }
+}

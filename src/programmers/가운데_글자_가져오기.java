784d637a765ded3693cbd6d4da58e06da1e8d763
plm9606 <plm9606@naver.com>
2020-04-14 00:37:41 +0900

    feat: 프로그래머스 - 가운데 글자 가져오기

---
+++ src/programmers/가운데_글자_가져오기.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class 가운데_글자_가져오기 {
+    public String solution(String s) {
+        String answer = "";
+        int len = s.length();
+
+        if(s.length()%2 ==0){
+            int mid = s.length()/2;
+            answer = s.substring(mid-1, mid+1);
+        }else {
+            int mid = (s.length()-1)/2;
+            answer = s.substring(mid, mid+1);
+        }
+        return answer;
+    }
+}

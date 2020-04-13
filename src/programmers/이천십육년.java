38206bb23f60bdfda4ce595d85d64a721cd956bb
plm9606 <plm9606@naver.com>
2020-04-14 00:38:28 +0900

    feat: 프로그래머스-2016년

---
+++ src/programmers/이천십육년.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class 이천십육년 {
+    public static void main(String[] args) {
+        System.out.println(solution(12,15));
+    }
+    public static String solution(int a, int b) {
+        int answer=b;
+        String[] days = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
+        int[] month = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+
+        for(int i=0; i<a; i++){
+            answer+=month[i];
+        }
+
+        answer = (answer+4)%7;
+        return days[answer];
+    }
+}

c7ffbb2615a8d549e409ea6f1f711ce510b96d80
plm9606 <plm9606@naver.com>
2020-04-16 13:18:03 +0900

    feat: 프로그래머스 - 멀쩡한 사각형
    https://lkhlkh23.tistory.com/154
    https://taesan94.tistory.com/55

---
+++ src/programmers/level2/멀쩡한_사각형.java
@@ -0,0 +1,15 @@
+package programmers.level2;
+
+public class 멀쩡한_사각형 {
+    public static void main(String[] args) {
+        System.out.println(solution(8,12));
+    }
+    public static long solution(int w,int h) {
+        long answer = 0;
+
+        for(int i=0; i<w; i++){
+            answer += (Long.valueOf(h)*i)/Long.valueOf(w);
+        }
+        return answer*2;
+    }
+}

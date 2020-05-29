ab4ac0e40e65a7a2d12c390eb1c01f97fd148a2f
plm9606 <plm9606@naver.com>
2020-05-30 01:01:13 +0900

    feat: 조합 알고리즘

--- src/algorithm/Permutation.java
+++ src/algorithm/Permutation.java
@@ -1,5 +1,8 @@
 package algorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Permutation {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4};
@@ -8,6 +11,7 @@ public class Permutation {
 
     public static void permutation(int[] arr, int[] order, int picked, int toPick){
         if(toPick == picked){
+            System.out.println(Arrays.toString(order));
             print(arr, order, toPick);
             return;
         }
@@ -31,5 +35,22 @@ public class Permutation {
             System.out.print(arr[idx] + " ");
         }
         System.out.println();
+    }
+
+    // n개 중에 r개를 뽑는 경우, 순서를 지키며
+    private static void perm(int[] arr, int[] output, boolean[] visited, int depth, int n, int r){
+        if(depth == r){
+            System.out.println(Arrays.toString(output));
+            return;
+        }
+
+        for(int i=0; i<n; i++){
+            if(visited[i] != true){ // 아직 방문하지 않았다면
+                visited[i] = true;
+                output[depth] = arr[i];
+                perm(arr, output, visited, depth+1, n, r);
+                visited[i] = false;
+            }
+        }
     }
 }

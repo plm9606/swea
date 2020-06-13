9cbb5edadc0a94e54077e3676a87d17025d7d9d5
plm9606 <plm9606@naver.com>
2020-06-13 12:32:33 +0900

    feat: boj 1463
    2부터 N까지 iterator가 돈다고 할 때, 배열의 값에 해당 index가 1이되려면
    해야 하는 최소한의 연산 횟수를 기록해놓을 수 있다.
    예를 들어 반복문 내에서 arr[2] = 1이 기록되어 있다면, arr[4]는 arr[i/2]
    + 1번을 기록해야한다.
    즉 2로 나눌 때 1번 연산이 필요한건 기록해 두었으니 4를 반으로 나누었을
    때의 연산횟수에 1번 더한 만큼 기록하는 것이다.
    먼저 반복문에서 arr[i] = arr[i-1] + 1번으로 기록해 두고 2나 3으로 나누어
    떨어지는 경우와 비교한다. 그리고 최솟값을 기록해둔다.
    예를 들어 i가 3일 경우 처음에는 arr[3]은 2일 것이다. (arr[1] = 0이므로)
    하지만 실제로 3은 3으로 나누면 한 번에 1로 만들 수 있다. 즉, 현재의
    arr[i]와 arr[i/3] + 1 중의 최솟값을 기록해야한다.

---
+++ src/baekjoon/Boj_1463.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Boj_1463 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        //int n = sc.nextInt();
+        int n = 14;
+        int arr[] = new int[n+1];
+
+        arr[1] = 0;
+
+        for(int i=2; i<=n; i++){
+            arr[i] = arr[i-1] +1;
+            if(i%2 == 0) arr[i] = Math.min(arr[i], arr[i/2]+1);
+            if(i%3 == 0) arr[i] = Math.min(arr[i], arr[i/3]+1);
+        }
+
+        System.out.println(arr[n]);
+    }
+}

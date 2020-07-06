4f95cce75982af2fbd812b259bf0448190ba57e0
plm9606 <plm9606@naver.com>
2020-07-06 23:54:36 +0900

    feat: nqueens 백트래킹

---
+++ src/algorithm/course/NQueen.java
@@ -0,0 +1,46 @@
+package algorithm.course;
+
+import java.util.Arrays;
+
+public class NQueen {
+
+    static  int N;
+    static  int[]Q;
+    static int count;
+    public static void main(String[] args) {
+         N = 8;
+         count = 0;
+        Q = new int[N+1];
+
+        queen(1);
+    }
+
+    private static void queen(int level){
+        if(level > N){
+            System.out.print(++count+": ");
+            System.out.println(Arrays.toString(Arrays.copyOfRange(Q,1,Q.length)));
+            return;
+        }
+
+        for(int i=1; i<=N; i++){
+            Q[level] = i;
+            if(promising(level)){
+                queen(level+1);
+            }
+        }
+    }
+
+    private static boolean promising(int level){
+        boolean res = true;
+        int cur = Q[level];
+
+        for(int i=1; i<level; i++){
+            if(Q[i] == cur || Math.abs(i-level)==Math.abs(Q[i]-cur)){
+                res = false;
+                break;
+            }
+        }
+
+        return res;
+    }
+}

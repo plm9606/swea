05c33b5f47b26c23af6b6bfae8a7452f8cf0a8eb
plm9606 <plm9606@naver.com>
2020-08-26 01:56:17 +0900

    feat: 프로그래머스 순위 solution 추가

--- src/programmers/순위.java
+++ src/programmers/순위.java
@@ -1,6 +1,8 @@
 package programmers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class 순위 {
     public int solution(int n, int[][] results) {
@@ -26,6 +28,7 @@ public class 순위 {
 //                }
 //            }
 //        }
+
         for(int k=1; k<=n; k++){
             for(int i=1; i<=n; i++){
                 for(int j=1; j<=n; j++){
@@ -54,5 +57,49 @@ public class 순위 {
 
         long answer = Arrays.stream(check).filter(item->item==0).count();
         return (int)answer-1;
+    }
+
+    public int solution_B(int n, int[][] results){
+        int answer = 0;
+        HashSet<Integer>[][] sets = new HashSet[n+1][2];
+
+        for(int i=0; i<results.length; i++){
+            int win = results[i][0];
+            int lose = results[i][1];
+
+            sets[win][1].add(lose);
+            sets[lose][0].add(win);
+        }
+
+        for(int i=1; i<=n; i++){
+            ArrayList<Integer> stronger = new ArrayList<>(sets[i][0]);
+            ArrayList<Integer> weaker = new ArrayList<>( sets[i][1]);
+
+            for(int k : stronger){
+                if(!sets[k][0].isEmpty()){
+                    for(int s:sets[k][0]){
+                        sets[i][0].add(s);
+                    }
+                }
+            }
+
+            for(int k: weaker){
+                if(!sets[k][1].isEmpty()){
+                    for(int s:sets[k][1]){
+                        sets[i][1].add(s);
+                    }
+                }
+            }
+        }
+
+        for(int i=1; i<=n; i++){
+            System.out.println(sets[i][0]);
+            System.out.println(sets[i][1]);
+            System.out.println("---------------------------------------------------------------------");
+        }
+
+
+
+        return answer;
     }
 }

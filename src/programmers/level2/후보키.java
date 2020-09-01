2a3b48fae0110a54df19fc46410a5aedfb6b4133
plm9606 <plm9606@naver.com>
2020-09-01 19:13:07 +0900

    feat: 프로그래머스 후보키

--- src/programmers/level2/후보키.java
+++ src/programmers/level2/후보키.java
@@ -1,75 +1,78 @@
 package programmers.level2;
 
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
 
-/**
- * 1. 각 속성별로 유일한 값만 존재하는지 검사한다.
- * 2. 중복이 있는 속성의 col index를 저장해둔다. {(1,2)(0,3,4)}
- * 3. 1개부터 n개를 선택하는 경우의 수를 완전탐색한다
- * 4. 각 속성의 중복되는 인덱스가 있는 경우 그룹 () 별 숫자가 겹치는지 확인한다.
- */
 public class 후보키 {
-    public static ArrayList<List<List<Integer>>> indexList;
-    public static void main(String[] args) {
-        String[][] r = {
-                {"100","ryan","music","2"},
-                {"200","apeach","math","2"},
-                {"300","tube","computer","3"},
-                {"400","con","computer","4"},
-                {"500","muzi","music","3"},
-                {"600","apeach","music","2"}
-        };
+    String[][] relation;
+    int answer = 0, columnCnt, rowCnt;
+    ArrayList<int[]> can = new ArrayList<>();
 
-        solution(r);
+    public int solution(String[][] relation) {
+        rowCnt = relation.length;
+        this.relation = relation;
+        columnCnt = relation[0].length;
 
-    }
-    public static int solution(String[][] relation) {
-        int answer = 0;
-        int relationCount = relation[0].length;
-        int tupleCount = relation.length;
-        indexList = new ArrayList<>();
-
-        for(int i=0; i<relationCount; i++){
-            Map<String, ArrayList<Integer>> map = new HashMap<>();
-            for(int j=0; j<tupleCount; j++){
-                if(map.keySet().contains(relation[j][i])){
-                    map.get(relation[j][i]).add(j);
-                }else {
-                    ArrayList<Integer> arr = new ArrayList<>();
-                    arr.add(j);
-                    map.put(relation[j][i], arr);
-                }
-            }
-            List<List<Integer>> duplicateIndexList = new ArrayList<>();
-            for(String key : map.keySet()){
-                if(map.get(key).size() > 1)
-                duplicateIndexList.add(map.get(key));
-            }
-            indexList.add(duplicateIndexList);
+        for (int i = 1; i <= columnCnt; i++) {
+            combination(columnCnt, new boolean[columnCnt], i, 0);
         }
 
-        combination(new boolean[relationCount], 1, 0);
         return answer;
     }
 
-    public static void combination(boolean[] bit, int toPick, int target){
-        if(toPick == 0){
-            // do something
-            for(int i=0; i<bit.length; i++){
-                if(!bit[i]){
-                    continue;
-                }
-                if(indexList.get(i).size() > 0){
+    public void combination(int n, boolean[] bit, int toChoice, int target) {
+        if (toChoice == 0) {
+            ArrayList<Integer> list = new ArrayList<>();
 
+            for (int i = 0; i < n; i++) {
+                if (bit[i]) {
+                    list.add(i);
                 }
+            }
+
+            if (list.size() > 1) {
+                HashSet<Integer> curKeys = new HashSet<>(list);
+                for (int[] cnadidateKey : can) {
+                    int cnt = 0;
+                    for (int key : cnadidateKey) {
+                        if (curKeys.contains(key)) {
+                            cnt++;
+                        }
+                    }
+                    if (cnt == cnadidateKey.length) return;
+                }
+            }
+
+            HashSet<String> set = new HashSet<>();
+            boolean flag = true;
+
+            for (int r = 0; r < rowCnt; r++) {
+                String[] row = relation[r];
+                String key = "";
+
+                for (int colIdx : list) {
+                    key += (row[colIdx] + " ");
+                }
+
+                if (set.contains(key)) {
+                    flag = false;
+                    break;
+                } else {
+                    set.add(key);
+                }
+            }
+
+            if (flag) {
+                answer++;
+                can.add(list.stream().mapToInt(i -> i).toArray());
             }
             return;
         }
 
-        bit[target] = true;
-        combination(bit, toPick-1, target+1);
-        bit[target] = false;
-        combination(bit, toPick, target+1);
+        for (int i = target; i < n; i++) {
+            bit[i] = true;
+            combination(n, bit, toChoice - 1, i + 1);
+            bit[i] = false;
+        }
     }
 }

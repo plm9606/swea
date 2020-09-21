10fd649164a88cb7dd7ab71fda1b39324414b5bb
plm9606 <plm9606@naver.com>
2020-09-22 00:14:20 +0900

    [boj] 15685 드래곤 커브 - 좌표 100까지 - 끝점을 구하는 방법 점사이의 거리가 아닌 커브의 선분을 타고 가정 먼 점

--- src/baekjoon/Boj_15685.java
+++ src/baekjoon/Boj_15685.java
@@ -4,12 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Boj_15685 {
     static int[] dy = {0, -1, 0, 1}, dx = {1, 0, -1, 0};
-    static int[][] arr = new int[100][100];
+    static int[][] arr = new int[101][101];
     static double rad = 90 * (float) (Math.PI / 180);
 
     public static void main(String[] args) throws IOException {
@@ -36,8 +35,8 @@ public class Boj_15685 {
         }
 
         int cnt = 0;
-        for (int i = 0; i < 99; i++) {
-            for (int j = 0; j < 99; j++) {
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
                 if (arr[i][j] == 1 && arr[i + 1][j] == 1 && arr[i][j + 1] == 1 && arr[i + 1][j + 1] == 1) cnt++;
             }
         }
@@ -51,35 +50,21 @@ public class Boj_15685 {
         return new int[]{(int) Math.round(rx), (int) Math.round(ry)};
     }
 
-    private static double getDistance(int x, int y, int cx, int cy) {
-        return (Math.pow((x - cx), 2) + Math.pow((y - cy), 2));
-    }
-
     static void makeCurve(int x, int y, int d, int g, ArrayList<int[]> curves) {
         if (g == 0) {
-            for (int[] c : curves) {
-                System.out.println(Arrays.toString(c));
-            }
-
-            System.out.println();
             return;
         }
-//        ArrayList<int[]> list = new ArrayList<>();
-        double max = -1;
+        int max = -1;
         int[] end = new int[2];
         int curDragonCurve = curves.size();
-        for (int i = 0; i < curDragonCurve; i++) {
+        for (int i = curDragonCurve - 1; i >= 0; i--) {
             int[] point = curves.get(i);
             if (point[0] == x && point[1] == y) continue;
             int[] curved = transformation(x, y, point[0], point[1]);
             curves.add(curved);
             arr[curved[1]][curved[0]] = 1;
-            double distance = getDistance(x, y, curved[0], curved[1]);
-            if (max < distance) {
-                max = distance;
-                end[0] = curved[0];
-                end[1] = curved[1];
-            }
+            end[0] = curved[0];
+            end[1] = curved[1];
         }
 
         makeCurve(end[0], end[1], d, g - 1, curves);

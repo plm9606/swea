df485c6570ee509dcbb12bfbb7f613c183d9de68
plm9606 <plm9606@naver.com>
2020-10-07 22:25:20 +0900

    [boj] 16637 괄호 추가하기 dfs 풀이 추가

--- src/baekjoon/Boj_16637.java
+++ src/baekjoon/Boj_16637.java
@@ -108,3 +108,55 @@ public class Boj_16637 {
         return acc;
     }
 }
+
+class Boj_16637_B {
+    static ArrayList<Integer> nums = new ArrayList<>();
+    static ArrayList<Character> symbols = new ArrayList<>();
+    static int N;
+    static int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("res/boj16637.txt"));
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        String expression = br.readLine();
+        for (int i = 0; i < N; i++) {
+            if (i % 2 == 0) {
+                nums.add(Integer.parseInt(expression.charAt(i) + ""));
+            } else {
+                symbols.add(expression.charAt(i));
+            }
+        }
+        dfs(0, nums.get(0));
+        System.out.println(max);
+    }
+
+    public static void dfs(int idx, int res) {
+        // 모든 연산자를 사용했으면
+        if (idx >= N / 2) {
+            max = Math.max(max, res);
+            return;
+        }
+        // 괄호를 치지 않는 경우 이전 값이랑 연산
+        int res1 = calc(res, symbols.get(idx), nums.get(idx + 1));
+        dfs(idx + 1, res1);
+
+        // 괄호를 치는 경우
+        if (idx + 1 < N / 2) {
+            // 다음 연산자에 괄호를 쳐서 이전 값과 연산
+            int res2 = calc(res, symbols.get(idx), calc(nums.get(idx + 1), symbols.get(idx + 1), nums.get(idx + 2)));
+            dfs(idx + 2, res2);
+        }
+    }
+
+
+    private static int calc(int a, char s, int b) {
+        if (s == '+') {
+            return a + b;
+        } else if (s == '-') {
+            return a - b;
+        }
+        return a * b;
+    }
+}

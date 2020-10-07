c642ce4241518a5481140be2daa29dabe82eaa21
plm9606 <plm9606@naver.com>
2020-10-07 22:22:54 +0900

    feat: dp를 이용한 조합 알고리즘

--- src/algorithm/Combination.java
+++ src/algorithm/Combination.java
@@ -1,34 +1,49 @@
 package algorithm;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Combination {
+    static int[][] sum = new int[30][30];
+
+    public static int c(int n, int r) {
+        if (sum[n][r] != 0) return sum[n][r];
+        if (n == r || r == 0) return 1;
+        else {
+            sum[n][r] = c(n - 1, r - 1) + c(n - 1, r);
+            return sum[n][r];
+        }
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {1, 2, 3, 4, 5};
+
+        ArrayList<Integer>[] lists = new ArrayList[10];
 
         // for(int i=1; i<=arr.length; i++)
         boolean[] b = new boolean[arr.length];
         combination(arr, b, 2, 0);
+
+        System.out.println(c(6, 2));
     }
 
-    public static void combination(int[] arr, boolean[] bit, int toChoice, int target){
-        if(toChoice == 0){
+    public static void combination(int[] arr, boolean[] bit, int toChoice, int target) {
+        if (toChoice == 0) {
             print(arr, bit);
             return;
         }
 
-        for(int i=target; i<arr.length; i++){
+        for (int i = target; i < arr.length; i++) {
             bit[i] = true;
-            combination(arr, bit, toChoice-1, i+1);
+            combination(arr, bit, toChoice - 1, i + 1);
             bit[i] = false;
         }
     }
 
-    public static void print(int[] arr, boolean[] bit){
-        for(int i=0; i< arr.length; i++){
+    public static void print(int[] arr, boolean[] bit) {
+        for (int i = 0; i < arr.length; i++) {
             boolean b = bit[i];
-            if(b){
-                System.out.print(arr[i]+ " ");
+            if (b) {
+                System.out.print(arr[i] + " ");
             }
         }
         System.out.println();
--- src/algorithm/ShiftCombination.java
+++ src/algorithm/ShiftCombination.java
@@ -1,24 +1,37 @@
 package algorithm;
 
 public class ShiftCombination {
-    static int[] num;
+
     public static void main(String[] args) {
-        int bit, n=5, m=3;
+        int bit, n = 5, m = 3;
 
-        bit = (1<<m) -1;
-        num = new int[bit];
+        bit = (1 << m) - 1;
 
-        while (bit < (1<< n)){
+        while (bit < (1 << n)) {
             func(bit, n);
-            int temp =bit | ((bit & -bit)-1);
+            int temp = bit | ((bit & -bit) - 1);
+
+            bit = (temp + 1) | (((~temp & -~temp) - 1) >> (getZeros(bit) + 1));
         }
+
     }
 
-    public static void func(int bit, int n){
-        int msb = 1 << (n-1);
-        while (msb != 0){
-            System.out.println(Boolean.valueOf(Integer.toString(msb&bit)));
+    public static void func(int bit, int n) {
+        int msb = 1 << (n - 1);
+        while (msb != 0) {
+            System.out.printf("%d ", (msb & bit));
             msb >>= 1;
         }
+        System.out.println();
+    }
+
+    public static int getZeros(int n) {
+        int num = 0;
+        while ((n & 1) == 0) {
+            num++;
+            n >>= 1;
+        }
+
+        return num;
     }
 }

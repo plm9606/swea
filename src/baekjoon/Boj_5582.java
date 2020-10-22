cb4f4759e214a093f6441e31a96e115963914989
plm9606 <plm9606@naver.com>
2020-10-22 20:02:15 +0900

    [boj] 5582

--- src/baekjoon/Boj_5582.java
+++ src/baekjoon/Boj_5582.java
@@ -25,5 +25,35 @@ public class Boj_5582 {
         }
 
         System.out.println(max);
+
+        solution(first, second);
+    }
+
+    static void solution(String s, String ss) {
+        //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String s = br.readLine(), ss = br.readLine();
+
+        s = "aaaaafdfegwbh";
+        ss = "dejjtu";
+
+        // dp[i][j] = s의 i번째 문자와 ss의 j번째 문자를 끝으로 하는 부분 문자열의 최대 길이
+        int[][] dp = new int[s.length()][ss.length()];
+        int max = 0;
+        for (int j = 0; j < ss.length(); j++) {
+            if (s.charAt(0) == ss.charAt(j)) dp[0][j] = 1;
+        }
+
+        for (int i = 1; i < s.length(); i++) {
+            for (int j = 0; j < ss.length(); j++) {
+                if (s.charAt(i) == ss.charAt(j)) {
+                    if (j == 0) dp[i][j] = 1;
+                    else dp[i][j] = 1 + dp[i - 1][j - 1];
+
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        System.out.println(max);
+
     }
 }

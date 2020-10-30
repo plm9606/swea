91abe4252382fa6fb523885c3345c2eb4ee602d2
plm9606 <plm9606@naver.com>
2020-10-30 12:14:00 +0900

    [boj] 14466 소가 길을 건너간 이유6
    유니온파인드 풀이 추가

--- src/baekjoon/Boj_14466B.java
+++ src/baekjoon/Boj_14466B.java
@@ -3,19 +3,16 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Boj_14466 {
+public class Boj_14466B {
     static int[] dy = {-1, 0, 1, 0}, dx = {0, 1, 0, -1};
-    static boolean visited[][], arr[][][];
+    static boolean arr[][][];
     static int n, cows[][];
-    static Queue<int[]> q;
+    static int[] parent;
 
     /**
-     * bfs
-     * bfs는 소당 한번만 돌려도 된다.
+     * 유니온파인드
      */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("res/boj14466.txt"));
@@ -26,8 +23,9 @@ public class Boj_14466 {
         int k = Integer.parseInt(st.nextToken());
         int r = Integer.parseInt(st.nextToken());
         int cnt = 0;
-        cows = new int[k][2];
+
         arr = new boolean[n][n][4];
+        cows = new int[k][2];
         for (int i = 0; i < r; i++) {
             st = new StringTokenizer(br.readLine());
             int[] v1 = {Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1};
@@ -49,32 +47,40 @@ public class Boj_14466 {
             cows[i][1] = Integer.parseInt(st.nextToken()) - 1;
         }
 
-        for (int i = 0; i < k - 1; i++) {
-            bfs(cows[i]);
-            for (int j = i + 1; j < k; j++) {
-                if (!visited[cows[j][0]][cows[j][1]]) {
-                    cnt++;
+        parent = new int[n * n];
+        for (int i = 0; i < parent.length; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int d = 0; d < 4; d++) {
+                    int yy = i + dy[d];
+                    int xx = j + dx[d];
+                    if (yy < 0 || yy >= n || xx < 0 || xx >= n || arr[i][j][d]) continue;
+                    merge(i * n + j, yy * n + xx);
                 }
+            }
+        }
+
+        for (int i = 0; i < k - 1; i++) {
+            for (int j = i + 1; j < k; j++) {
+                if (find(cows[i][0] * n + cows[i][1]) != find(cows[j][0] * n + cows[j][1])) cnt++;
             }
         }
 
         System.out.println(cnt);
     }
 
-    static void bfs(int[] from) {
-        visited = new boolean[n][n];
-        q = new LinkedList<>();
-        q.add(from);
-        visited[from[0]][from[1]] = true;
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            for (int d = 0; d < 4; d++) {
-                int yy = cur[0] + dy[d];
-                int xx = cur[1] + dx[d];
-                if (yy < 0 || yy >= n || xx < 0 || xx >= n || visited[yy][xx] || arr[cur[0]][cur[1]][d]) continue;
-                visited[yy][xx] = true;
-                q.add(new int[]{yy, xx});
-            }
-        }
+    static void merge(int a, int b) {
+        a = find(a);
+        b = find(b);
+        parent[b] = a;
+    }
+
+    static int find(int a) {
+        if (parent[a] == a) return a;
+        parent[a] = find(parent[a]);
+        return parent[a];
     }
 }
